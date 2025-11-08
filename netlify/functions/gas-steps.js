b7b0f9f1af8bfec52eb803c0b2925e43c3d64671
villagran-gif <villagran@clinyco.cl>
2025-11-08 02:52:15 -0300

    Update gas-steps.js

--- netlify/functions/gas-steps.js
+++ netlify/functions/gas-steps.js
@@ -1,7 +1,7 @@
 export async function handler(event){
   const deal = event.queryStringParameters.deal_id || '';
   const key  = event.queryStringParameters.key || 'preoperatorio-v1';
-  const GAS_BASE = process.env.https://script.google.com/macros/s/AKfycbxlVy81nqAgA88zgGbiDAkW5mQhEev-UWMWsgRwxZ5YkAhP9xGMpvi3HVr715OwFADi/exec
+  const GAS_BASE = process.env.https://https://script.google.com/macros/s/AKfycbzlvP3lGBviiQvRi3wFiKz0FnnKhoY1n11YUnA3av9KfYFvvFrgqCZV6__1CqKB4rhx/exec
   const url = `${GAS_BASE}?api=1&action=listPublished&key=${encodeURIComponent(key)}&deal_id=${encodeURIComponent(deal)}`;
   try{
     const resp = await fetch(url);
